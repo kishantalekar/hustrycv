31b06289b525ec0965ff4fd53319c032ef4aa6ae
kishan talekar <115180515+kishantalekar@users.noreply.github.com>
2025-06-24 22:23:13 +0530

    build: update version to 1.1.8 and changelog for new features

--- src/constants/changelog.ts
+++ src/constants/changelog.ts
@@ -7,8 +7,9 @@ export interface ChangeLogItem {
 export const CHANGELOG: ChangeLogItem[] = [
   {
     version: '1.1.8',
-    date: '2025-10-01',
+    date: '2025-06-25',
     changes: [
+      'Added 20+ resume templates',
       'Added Hobbies section',
       'Added Strengths section',
       'Added References section',
